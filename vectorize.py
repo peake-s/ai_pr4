3997eb7701cb61516e6f570de8559ee736565545
peake-s <peakes@vcu.edu>
2022-11-03 10:02:24 -0400

    bones

---
+++ vectorize.py
@@ -0,0 +1,47 @@
+
+
+def tokeninze(paragraphs):
+    #loop through the paragraph put unique words in a dictionary
+    unique_words = {}   
+    return unique_words
+    
+
+def remove_special_characters(paragraphs):
+    #remove '.', ',','?', '"','"', '(',')', '<br', '/>', '''
+    special_characters = ['.', ',','?', '"','"', '(',')', '<br', '/>', ","]
+    removed_characters = []
+    
+    return removed_characters
+    
+
+def remove_numbers(paragraphs):
+    #regex to remove digits
+    pass
+
+def to_lower(paragraphs):
+    #call tolower funcition
+    pass
+
+def remove_stop_words(paragraphs,stopwords):
+    #load stopwords into a list
+    #loop through list and use python remove function
+    #return file with no stop words
+    pass
+
+def stemmer(paragraphs):
+    pass
+
+def frequency(paragraphs):
+    #count the frequency of every word
+    #return dictionary
+    pass
+
+def write(freqs):
+    #write the frequency matrix to a file
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()

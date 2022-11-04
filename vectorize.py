b8c3af19bc2c8373ade428b4aaddd77a245169eb
Sammy Peake <peakes@vcu.edu>
2022-11-03 21:04:54 -0400

    doing a lot

--- vectorize.py
+++ vectorize.py
@@ -1,63 +1,81 @@
-from pathlib import Path
+from . import PorterStemmer
 
-def tokeninze(paragraphs):
-    #loop through the paragraph put unique words in a dictionary
-    unique_words = set()   
+def tokenize(paragraphs):
     words = paragraphs.split()
-    for word in words:
-        #for word in paragraph:
-        unique_words.add(word)
 
-    return unique_words
+    return words
     
 
 def remove_special_characters(paragraphs):
-    #remove '.', ',','?', '"','"', '(',')', '<br', '/>', '''
-    special_characters = ['.', ',','?', '"','"', '(',')', '<br', '/>', ","]
-    removed_characters = []
+    special_characters = ['.', ',','?', '"','"', "(",")", "<br", "/>", ",","'","'",'!']
+   
+    removed_characters = paragraphs
+    for i in special_characters:
+        removed_characters = removed_characters.replace(i, '')
     
     return removed_characters
     
 
 def remove_numbers(paragraphs):
-    #regex to remove digits
+    special_characters = ['0','1','2','3','4','5','6','7','8','9']
+   
+    removed_digits= paragraphs
+    for i in special_characters:
+        removed_digits = removed_digits.replace(i, '')
+    
+    return removed_digits
+
+def to_lower(paragraph):
+    data = [word.lower() for word in paragraph]
+    return data
+
+def remove_stop_words(words,stopwords):
+    removed = []
+    removed = [word for word in words if word not in stopwords]
+
+    return removed
+
+def stemmer(paragraph):
+    port = PorterStemmer()
     pass
 
-def to_lower(paragraphs):
-    #call tolower funcition
-    pass
+def frequency(words):
+    unique_words = {}   
+    
+    for word in words:
+        if word in unique_words:
+            unique_words[word] += 1
+        else:
+            unique_words[word] = 1
 
-def remove_stop_words(paragraphs,stopwords):
-    #load stopwords into a list
-    #loop through list and use python remove function
-    #return file with no stop words
-    pass
-
-def stemmer(paragraphs):
-    pass
-
-def frequency(paragraphs):
-    #count the frequency of every word
-    #return dictionary
-    pass
 
 def write(freqs):
     #write the frequency matrix to a file
     pass
 
 def read_file(filename):
-    file_data = ""
-    file_data= Path(filename).read_text()
-    #with open(filename, 'r') as f:
-    #    file_data = f.read()
+    file_data = []
+    with open(filename, 'r') as f:
+        #file_data = (f.readlines())
+        file_data = f.read()
 
-    return file_data
+    #paragraphs
+    splat = file_data.split('\n')
+
+    return splat
 
 def main():
-    fd = read_file('Project4_paragraphs.txt')
-    print(type(fd))
-    token = tokeninze(fd)
-    print(token)
+    paragraph_list = read_file('Project4_paragraphs.txt','\n')
+    stop_words = read_file('Project4_stop_words.txt','\n')
+    ported_stemmed_pars = []
+
+    for paragraph in paragraph_list:
+        paragraph = remove_special_characters(paragraph)
+        paragraph = remove_numbers(paragraph)
+        tokenized_par = tokenize(paragraph)
+        lower_toke = to_lower(tokenized_par)
+        stop_words_removed = remove_stop_words(lower_toke,stop_words)
+
 
 if __name__ == '__main__':
     main()
